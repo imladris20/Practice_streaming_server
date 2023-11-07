3a1765b6faef63310a1904db7d056db8513b4b1d
Po Lien Lin <polien.joe.lin@gmail.com>
2023-11-07 15:22:57 +0800

    initilize

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const webrtc = require("wrtc");
+const cors = require("cors");
+
+let senderStream;
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/consumer", async ({ body }, res) => {
+  const peer = new webrtc.RTCPeerConnection({
+    iceServers: [
+      {
+        urls: "stun:stun.stunprotocol.org",
+      },
+    ],
+  });
+  const desc = new webrtc.RTCSessionDescription(body.sdp);
+  await peer.setRemoteDescription(desc);
+  senderStream
+    .getTracks()
+    .forEach((track) => peer.addTrack(track, senderStream));
+  const answer = await peer.createAnswer();
+  await peer.setLocalDescription(answer);
+  const payload = {
+    sdp: peer.localDescription,
+  };
+
+  res.json(payload);
+});
+
+app.post("/broadcast", async ({ body }, res) => {
+  const peer = new webrtc.RTCPeerConnection({
+    iceServers: [
+      {
+        urls: "stun:stun.stunprotocol.org",
+      },
+    ],
+  });
+  peer.ontrack = (e) => handleTrackEvent(e, peer);
+  const desc = new webrtc.RTCSessionDescription(body.sdp);
+  await peer.setRemoteDescription(desc);
+  const answer = await peer.createAnswer();
+  await peer.setLocalDescription(answer);
+  const payload = {
+    sdp: peer.localDescription,
+  };
+
+  res.json(payload);
+});
+
+function handleTrackEvent(e, peer) {
+  senderStream = e.streams[0];
+}
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // 或者 "*" 允許所有來源
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+app.listen(5000, () => console.log("server started on port 5000"));
